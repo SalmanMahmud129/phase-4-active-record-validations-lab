5d016d1d248334ebd12d3160dfca4e76702e3ae2
Salman Mahmud <salman_mahmud@hotmail.com>
2022-11-23 14:13:13 -0500

    commit tree 6b5072f5c8dc4ab97fc7bc258e25d4eba42df869

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :click_bait?
+
+
+    def click_bait?
+        
+        keywords = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+         if keywords.any? {|word| word.match(title)}
+            true
+         else
+            errors.add(:title, "Not click-baity enough")
+         end
+        # if title == "True Facts" 
+        #     errors.add(:title, "Title not allowed")
+        # end
+
+        # title.
+    end
 end
